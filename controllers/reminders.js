7039c15c0c49de2a859e2947b5c957e57980f045
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 21:51:41 -0500

    Definiendo metodo createReminder

--- controllers/reminders.js
+++ controllers/reminders.js
@@ -1,6 +1,22 @@
 const Reminders = require('../models/reminders');
 const {Op} = require('sequelize');
+const Reminder = require('../models/reminders');
 
+class ReminderController {
+    // Usando sequalize en memoria
+    async createReminder(req, res) {
+        try {
+            const { body } = req;
+            const data = await ReminderModel.findAll({});
+
+            body.id = data.length + 1;
+            const response = await ReminderModel.create(body);
+            res.status(201).json(response);
+        } catch (err) {
+            throw new Error(err.message);
+        }
+    }
+}
 
 module.exports = {
 
