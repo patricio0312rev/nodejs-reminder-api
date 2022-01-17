f28e6a60ebfbeba70ea152c7cf0f2146d27bfbf1
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 21:48:53 -0500

    Definiendo rutas en routes/reminder.js

--- routes/reminders.js
+++ routes/reminders.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
 const controller = require('../controllers/reminders');
 
+router.post('/', controller.createReminder);
+router.get('/', controller.getAllReminders);
+router.get('/:id', controller.getReminderById);
+router.put('/:id', controller.updateReminderById);
+router.patch('/:id', controller.updateReminderById);
+router.delete('/:id', controller.updateReminderById);
+
 module.exports = router;
